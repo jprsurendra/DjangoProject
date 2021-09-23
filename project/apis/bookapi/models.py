c69b3f2d9b0acfede6cd4ea8a2ce52178119eb81
Surendra Singh Rathore <jpr_surendra@yahoo.co.in>
2021-09-23 16:05:07 +0530

    add models

--- project/apis/bookapi/models.py
+++ project/apis/bookapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    book_id=models.IntegerField(primary_key=True)
+    title=models.CharField(max_length=200)
+    author=models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'tbl_books'
+
+    def __str__(self):
+        return self.title
--- project/apis/noteapi/models.py
+++ project/apis/noteapi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'tbl_notes'
+
+    def __str__(self):
+        return self.title
